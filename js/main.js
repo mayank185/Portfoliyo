718766561b8b4d10ad094960579db77a556a36ae
Your Name <mayankmoolchandani@gmail.com>
2019-05-11 23:12:54 +0530

    [ADD]text animation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,17 @@
-jQuery(document).ready(function($){
-	//open menu
-	$('.cd-menu-trigger').on('click', function(event){
-		event.preventDefault();
-		$('#cd-main-content').addClass('move-out');
-		$('#main-nav').addClass('is-visible');
-		$('.cd-shadow-layer').addClass('is-visible');
-	});
-	//close menu
-	$('.cd-close-menu').on('click', function(event){
-		event.preventDefault();
-		$('#cd-main-content').removeClass('move-out');
-		$('#main-nav').removeClass('is-visible');
-		$('.cd-shadow-layer').removeClass('is-visible');
-	});
 
-	//clipped image - blur effect
-	set_clip_property();
-	$(window).on('resize', function(){
-		set_clip_property();
+	$(window).on("load", function () {
+		$('.ml3').each(function(){
+			$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+		  });
+		  
+		  anime.timeline({loop: false})
+			.add({
+			  targets: '.ml3 .letter',
+			  opacity: [0,1],
+			  easing: "easeInOutQuad",
+			  duration: 100,
+			  delay: function(el, i) {
+				return 100 * (i+1)
+			  }
+			});
 	});
-
-	function set_clip_property() {
-		var $header_height = $('.cd-header').height(),
-			$window_height = $(window).height(),
-			$header_top = $window_height - $header_height,
-			$window_width = $(window).width();
-		$('.cd-blurred-bg').css('clip', 'rect('+$header_top+'px, '+$window_width+'px, '+$window_height+'px, 0px)');
-	}
-});
